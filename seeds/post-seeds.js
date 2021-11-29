86c558fac691a0b48eb8a956fba81a4aa01dcd22
Tari <bota.maral@gmail.com>
2021-11-28 20:27:29 -0500

    add seeds

---
+++ seeds/post-seeds.js
@@ -0,0 +1,47 @@
+const { Post } = require('../models');
+
+const postData = [
+    {
+        title:'Lorem ipsum dolor sit amet.',
+        post_text:'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        user_id: 4
+    },
+    {
+        title:'Lorem ipsum dolor sit amet.',
+        post_text: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        user_id:2
+    },
+    {
+        title:'Lorem ipsum dolor sit amet.',
+        post_text:'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        user_id: 5
+    },
+    {
+        title:'Lorem ipsum dolor sit amet.',
+        post_text:'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        user_id:1
+    },
+    {
+        title: 'Lorem ipsum dolor sit amet.',
+        post_text:'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        user_id:3
+    },
+    {
+        title: 'Lorem ipsum dolor sit amet.',
+        post_text:'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        user_id:5
+    },
+    {
+        title:'Lorem ipsum dolor sit amet.',
+        post_text:'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        user_id:4
+    },
+    {
+        title:'Lorem ipsum dolor sit amet.',
+        post_text:'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        user_id: 2
+    }
+];
+const seedPosts = () => Post.bulkCreate(postData);
+
+module.exports = seedPosts;
---
+++ seeds/user-seeds.js
@@ -0,0 +1,32 @@
+const { User } = require('../models');
+
+const userData = [
+    {
+        username: 'Josef',
+        email: 'josef@gmail.com',
+        password: 'pass456'
+    },
+    {
+        username: 'Josefina',
+        email: 'josefina@gmail.com',
+        password: 'pass456'
+    },
+    {
+        username: 'Larry',
+        email: 'larry@gmail.com',
+        password: 'pass456'
+    },
+    {
+        username: 'Nick',
+        email: 'nick@gmail.com',
+        password: 'pass456'
+    },
+    {
+        username: 'Alissa',
+        email: 'alissa@gmail.com',
+        password: 'pass456'
+    }
+];
+const seedUsers = () => User.bulkCreate(userData);
+
+module.exports = seedUsers;
