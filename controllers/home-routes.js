5aa5ee4db0f3f88c55658bffe367e9195d7893d0
Tari <bota.maral@gmail.com>
2021-11-30 12:22:38 -0500

    update readme file

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -73,8 +73,7 @@ router.get('/post/:id', (req, res) => {
           }
           const post = dbPostData.get({ plain: true });
           res.render('single-post', {
-            post,
-            loggedIn: req.session.loggedIn
+            post
           });
     })
     .catch(err => {
